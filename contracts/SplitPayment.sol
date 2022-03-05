518b3f43a8d811f041faef0d599b9de92f2a5e0c
Carlos Calderon <cdcalderon@gmail.com>
2022-03-04 22:24:03 -0600

    add split payment contract

---
+++ contracts/SplitPayment.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.9;
+
+contract SplitPayment {
+    function send(address payable[] memory to, uint256[] memory amount)
+        public
+    {}
+}
