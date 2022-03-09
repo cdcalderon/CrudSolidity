206cda4f8bdbc355c44a8f15115f32bf2791029d
Carlos Calderon <cdcalderon@gmail.com>
2022-03-08 18:57:41 -0600

    add contructor

--- contracts/SplitPayment.sol
+++ contracts/SplitPayment.sol
@@ -5,6 +5,12 @@ pragma solidity ^0.8.9;
 error SplitPayment__InvalidArguments();
 
 contract SplitPayment {
+    address public owner;
+
+    constructor(address _owner) {
+        owner = _owner;
+    }
+
     function send(address payable[] memory to, uint256[] memory amount)
         public
         payable
