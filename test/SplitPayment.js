79b17a518ab87aa2e42b10c4b5cb8253630631e4
Carlos Calderon <cdcalderon@gmail.com>
2022-03-12 16:10:56 -0600

    implement unit test for split payments happy path

--- test/SplitPayment.js
+++ test/SplitPayment.js
@@ -2,40 +2,51 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
 describe("SplitPayment", function () {
-  let splitPayment;
-  let accounts;
-  let deployer;
+  let splitPayment, accounts, deployer;
+
   before(async () => {
     accounts = await ethers.getSigners();
-    deployer = accounts[1];
+    deployer = accounts[0];
     const SplitPayment = await ethers.getContractFactory("SplitPayment");
-
-    console.log(deployer.address);
-
-    splitPayment = await SplitPayment.deploy(accounts[1].address);
+    console.log("deployer address: ", deployer.address);
+    splitPayment = await SplitPayment.deploy(deployer.address);
   });
 
   it("Should create a SplitPayment", async () => {
-    // const recipients = [accounts[1], accounts[2], accounts[3]];
-    // const amounts = [40, 20, 30];
-    // const initialBalances = await Promise.all(
-    //   recipients.map((recipient) => {
-    //     return web3.eth.getBalance(recipient);
-    //   })
-    // );
-    // await splitPayments.send(recipients, amounts, {
-    //   from: accounts[0],
-    //   value: 90,
-    // });
-    // const finalBalances = await Promise.all(
-    //   recipients.map((recipient) => {
-    //     return web3.eth.getBalance(recipient);
-    //   })
-    // );
-    // recipients.forEach((_item, i) => {
-    //   const finalBalance = web.utils.toBN(finalBalances[i]);
-    //   const initialBalance = web.utils.toBN(initialBalances[i]);
-    //   expect(finalBalance, sub(initialBalance).toNumber() === amounts[i]);
-    // });
+    const recipients = [accounts[1], accounts[2], accounts[3]];
+
+    const addresses = [
+      accounts[1].address,
+      accounts[2].address,
+      accounts[3].address,
+    ];
+
+    const amounts = [40, 20, 30];
+    const initialBalances = await Promise.all(
+      recipients.map((recipient) => {
+        return recipient.getBalance();
+      })
+    );
+
+    await splitPayment.connect(deployer).send(addresses, amounts, {
+      from: deployer.address,
+      value: 90,
+    });
+
+    const finalBalances = await Promise.all(
+      recipients.map((recipient) => {
+        return recipient.getBalance();
+      })
+    );
+
+    recipients.forEach((_item, i) => {
+      const finalBalance = finalBalances[i];
+      const initialBalance = initialBalances[i];
+
+      console.log("finalBalance ", finalBalance);
+      console.log("initialBalance ", initialBalance);
+      console.log("amount ", amounts[i]);
+      expect(finalBalance.sub(initialBalance) === amounts[i]);
+    });
   });
 });
