cfa0ed4fc9b17b629830b8dc4c3454a680f198fa
Carlos Calderon <cdcalderon@gmail.com>
2022-03-11 19:17:55 -0600

    fix unit tests

--- test/EtherWallet.js
+++ test/EtherWallet.js
@@ -2,10 +2,13 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
 describe("EtherWallet", function () {
+  let deployer;
   let crud;
   before(async () => {
+    accounts = await ethers.getSigners();
+    deployer = accounts[1];
     const EtherWallet = await ethers.getContractFactory("EtherWallet");
-    crud = await EtherWallet.deploy();
+    crud = await EtherWallet.deploy(deployer.address);
   });
 
   it("Should create a wallet", async () => {});
--- test/SplitPayment.js
+++ test/SplitPayment.js
@@ -2,11 +2,40 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
 describe("SplitPayment", function () {
-  let crud;
+  let splitPayment;
+  let accounts;
+  let deployer;
   before(async () => {
-    const EtherWallet = await ethers.getContractFactory("SplitPayment");
-    crud = await EtherWallet.deploy();
+    accounts = await ethers.getSigners();
+    deployer = accounts[1];
+    const SplitPayment = await ethers.getContractFactory("SplitPayment");
+
+    console.log(deployer.address);
+
+    splitPayment = await SplitPayment.deploy(accounts[1].address);
   });
 
-  it("Should create a SplitPayment", async () => {});
+  it("Should create a SplitPayment", async () => {
+    // const recipients = [accounts[1], accounts[2], accounts[3]];
+    // const amounts = [40, 20, 30];
+    // const initialBalances = await Promise.all(
+    //   recipients.map((recipient) => {
+    //     return web3.eth.getBalance(recipient);
+    //   })
+    // );
+    // await splitPayments.send(recipients, amounts, {
+    //   from: accounts[0],
+    //   value: 90,
+    // });
+    // const finalBalances = await Promise.all(
+    //   recipients.map((recipient) => {
+    //     return web3.eth.getBalance(recipient);
+    //   })
+    // );
+    // recipients.forEach((_item, i) => {
+    //   const finalBalance = web.utils.toBN(finalBalances[i]);
+    //   const initialBalance = web.utils.toBN(initialBalances[i]);
+    //   expect(finalBalance, sub(initialBalance).toNumber() === amounts[i]);
+    // });
+  });
 });
