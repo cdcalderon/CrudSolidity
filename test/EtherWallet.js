ea1d16ca0816caea9ae1f87050514b07b04a0edb
Carlos Calderon <cdcalderon@gmail.com>
2022-03-15 20:45:44 -0500

    add unit test

--- test/EtherWallet.js
+++ test/EtherWallet.js
@@ -3,13 +3,21 @@ const { ethers } = require("hardhat");
 
 describe("EtherWallet", function () {
   let deployer;
-  let crud;
+  let etherWallet;
   before(async () => {
     accounts = await ethers.getSigners();
-    deployer = accounts[1];
+    deployer = accounts[0];
     const EtherWallet = await ethers.getContractFactory("EtherWallet");
-    crud = await EtherWallet.deploy(deployer.address);
+    etherWallet = await EtherWallet.deploy(deployer.address);
   });
 
-  it("Should create a wallet", async () => {});
+  it("Should set accounts[0] as owner", async () => {
+    const owner = await etherWallet.owner();
+
+    expect(owner).to.equal(deployer.address);
+  });
+  // it("Should create a wallet", async () => {
+  //   const sendAmount = 1;
+  // await etherWallet.connect(deployer).send(accounts[1].address, );
+  // });
 });
