445d5101d48327ee88fed9e1c5d8af1a77ca758e
Carlos Calderon <cdcalderon@gmail.com>
2022-03-22 01:16:03 -0500

    remove unneeded property

--- contracts/Deed.sol
+++ contracts/Deed.sol
@@ -6,22 +6,15 @@ error Deed__InvalidAmount();
 contract Deed {
     address public lawyer;
     address payable public beneficiary;
-    uint256 public amount;
     uint256 public earliest;
 
     constructor(
         address _lawyer,
         address payable _beneficiary,
-        uint256 _amount,
         uint256 _fromNow
     ) payable {
-        if (amount != msg.value) {
-            revert Deed__InvalidAmount();
-        }
-
         lawyer = _lawyer;
         beneficiary = _beneficiary;
-        amount = _amount;
         _fromNow = _fromNow;
     }
 }
