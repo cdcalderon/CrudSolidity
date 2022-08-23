5bbbe3fec711965b8574ecd754296e22cee2822c
Carlos Calderon <cdcalderon@gmail.com>
2022-08-23 00:29:07 -0500

    add Crud contract

---
+++ contracts/Crud.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+contract Crud {
+    struct User {
+        uint256 id;
+        string name;
+    }
+    User[] public users;
+    uint256 public nextId;
+
+    function create(string memory name) public {
+        users.push(User({id: nextId, name: name}));
+        nextId++;
+    }
+
+    function read(uint256 id) public view returns (uint256, string memory) {
+        for (uint256 i = 0; i < users.length; i++) {
+            if (users[i].id == id) {
+                return (users[i].id, users[i].name);
+            }
+        }
+        return (0, "");
+    }
+}
