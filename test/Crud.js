adabc4c2d1cc36276738c8df42168018e3ff3e27
Carlos Calderon <cdcalderon@gmail.com>
2022-08-28 20:57:14 -0500

    add initial unit test setup

---
+++ test/Crud.js
@@ -0,0 +1,18 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("Crud", function () {
+  let crud;
+  before(async () => {
+    const Crud = await ethers.getContractFactory("Crud");
+    crud = await Crud.deploy();
+  });
+
+  it("Should create a new user", async () => {
+    await crud.create("Myself");
+    const user = await crud.read(1);
+
+    assert(user[0].toNumber() === 1);
+    assert(user[1] === "Myself");
+  });
+});
