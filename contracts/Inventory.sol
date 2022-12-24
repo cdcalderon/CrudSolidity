439d982362bb842c9ca548114556a2f43cb393b8
Carlos Calderon <cdcalderon@gmail.com>
2022-12-24 16:38:25 -0600

    add Inventory contract

---
+++ contracts/Inventory.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.17;
+
+contract Inventory {
+    mapping(uint256 => int256) quantities;
+
+    function addItem(uint256 itemId, int256 quantity) public {
+        quantities[itemId] = quantity;
+    }
+
+    function getQuantity(uint256 itemId) public view returns (int256) {
+        return quantities[itemId] != 0 ? int256(quantities[itemId]) : -1;
+    }
+}
