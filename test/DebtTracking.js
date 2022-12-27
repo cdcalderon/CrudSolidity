545c18fd4f21be4bbaa4fd698064c405c09a3fe3
Carlos Calderon <cdcalderon@gmail.com>
2022-12-27 14:54:16 -0600

    add unit tests

--- test/DebtTracking.js
+++ test/DebtTracking.js
@@ -1,15 +1,13 @@
 const { assert } = require("chai");
 const { ethers } = require("hardhat");
 
-describe("DebtTracking", () => {
+describe("DebtTracking", async () => {
   let debtTracking, addr1, addr2, addr3;
-
+  accounts = await ethers.getSigners();
+  addr1 = accounts[0];
+  addr2 = accounts[1];
+  addr3 = accounts[2];
   beforeEach(async () => {
-    accounts = await ethers.getSigners();
-    addr1 = accounts[0];
-    addr2 = accounts[1];
-    addr3 = accounts[2];
-
     const DebtTracking = await ethers.getContractFactory("DebtTracking");
     debtTracking = await DebtTracking.deploy();
   });
@@ -22,5 +20,50 @@ describe("DebtTracking", () => {
     let currentDebt = await debtTracking.getDebt(addr1.address, addr2.address);
     console.log(currentDebt);
     assert.equal(currentDebt, debtAmount);
+  });
+
+  it("should add debt between addr1 and addr3", async () => {
+    await debtTracking.addDebt(addr1.address, addr3.address, 15);
+    assert.equal(
+      await debtTracking.getDebt(addr1.address, addr3.address),
+      15,
+      "addr3 should owe addr1 15 ether"
+    );
+  });
+
+  it("should add more debt between addr1 and addr2", async () => {
+    await debtTracking.addDebt(addr1.address, addr2.address, 15);
+    assert.equal(
+      (await debtTracking.getDebt(addr1.address, addr2.address)).toNumber(),
+      25,
+      "addr2 should owe addr1 25 ether"
+    );
+  });
+
+  it("addr3 can pay back addr1", async () => {
+    await debtTracking.payDebt(addr1.address, addr3.address, 10);
+    assert.equal(
+      (await debtTracking.getDebt(addr1.address, addr3.address)).toNumber(),
+      5,
+      "addr3 should owe addr1 5 ether"
+    );
+  });
+
+  it("addr3 can pay back addr1 again", async () => {
+    await debtTracking.payDebt(addr1.address, addr3.address, 5);
+    assert.equal(
+      await debtTracking.getDebt(addr1.address, addr3.address),
+      0,
+      "addr3 should owe addr1 0 ether"
+    );
+  });
+
+  it("addr2 can pay back addr1 again", async () => {
+    await debtTracking.payDebt(addr1.address, addr2.address, 10);
+    assert.equal(
+      await debtTracking.getDebt(addr1.address, addr2.address),
+      15,
+      "addr2 should owe addr1 15 ether"
+    );
   });
 });
