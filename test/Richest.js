d9fcf1c45229685063d4462ac3a2891d447d8741
Carlos Calderon <cdcalderon@gmail.com>
2022-12-30 17:01:53 -0600

    add unit test

--- test/Richest.js
+++ test/Richest.js
@@ -2,10 +2,11 @@ const { assert } = require("chai");
 const { ethers } = require("hardhat");
 
 describe("Richest", async function () {
-  let richest, acc0, acc1;
+  let richest, deployer, acc1, acc2;
 
   before(async () => {
     accounts = await ethers.getSigners();
+    deployer = accounts[0];
     acc0 = accounts[1];
     acc1 = accounts[2];
     const Richest = await ethers.getContractFactory("Richest");
@@ -21,8 +22,17 @@ describe("Richest", async function () {
     console.log(acc0);
     await richest
       .connect(acc0)
-      .becomeRichest({ from: acc0.address, value: 1e10 });
+      .becomeRichest({ from: acc0.address, value: 1e3 });
     const richestAddress = await richest.getRichest();
     assert.equal(acc0.address, richestAddress);
+  });
+
+  it("richest address cannot withdraw", async () => {
+    const balance = await ethers.provider.getBalance(deployer.address);
+    await richest.connect(acc0).withdraw({ from: acc0.address });
+    const newBalance = await ethers.provider.getBalance(deployer.address);
+
+    //console.log(balance.toString(), newBalance.toString());
+    assert.equal(newBalance.toString(), balance.toString());
   });
 });
