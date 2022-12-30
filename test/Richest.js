a17c47bbc081a446d40cd5021227e27cc97f3c3a
Carlos Calderon <cdcalderon@gmail.com>
2022-12-30 15:44:00 -0600

    add unit test

--- test/Richest.js
+++ test/Richest.js
@@ -1,14 +1,28 @@
-describe("Richest", async () => {
-  let richest;
-  accounts = await ethers.getSigners();
-  const acc0 = accounts[1];
-  const acc1 = accounts[2];
+const { assert } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("Richest", async function () {
+  let richest, acc0, acc1;
+
   before(async () => {
-    const Richest = await ethers.getContractFactory("DebtTracking");
+    accounts = await ethers.getSigners();
+    acc0 = accounts[1];
+    acc1 = accounts[2];
+    const Richest = await ethers.getContractFactory("Richest");
     richest = await Richest.deploy();
   });
 
-  it("should have default richest address as 0 address", async () => {
-    assert.equal(0, await instance.getRichest());
+  it("Should create a new user", async () => {
+    console.log(await richest.getRichest());
+    assert.equal(0, await richest.getRichest());
+  });
+
+  it("account0 can become the richest", async () => {
+    console.log(acc0);
+    await richest
+      .connect(acc0)
+      .becomeRichest({ from: acc0.address, value: 1e10 });
+    const richestAddress = await richest.getRichest();
+    assert.equal(acc0.address, richestAddress);
   });
 });
