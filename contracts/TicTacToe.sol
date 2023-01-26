b03afd760823eef1c93f08af1ccddefe384fe30a
carlos calderon <cdcalderon@gmail.com>
2023-01-25 22:49:08 -0600

    implement winningPlayerShape function

--- contracts/TicTacToe.sol
+++ contracts/TicTacToe.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.24;
+pragma solidity ^0.8.17;
  
  contract TicTacToe {
      // Addresses of the players that are competing:
@@ -15,4 +15,34 @@ pragma solidity ^0.4.24;
          player1 = msg.sender;
          player2 = _player2;
      }
+
+     function winningPlayerShape() public view returns (SquareState) {
+        // Columns
+        if(board[0][0] != SquareState.Empty && board[0][0] == board[0][1] && board[0][2]){
+            return board[0][0];
+        }
+        if(board[1][0] != SquareState.Empty && board[1][0] == board[1][1] && board[1][2]){
+            return board[1][0];
+        }
+        if(board[2][0] != SquareState.Empty && board[2][0] == board[2][1] && board[2][2]){
+            return board[2][0];
+        }
+        // Rows
+         if(board[0][0] != SquareState.Empty && board[0][0] == board[1][0] && board[2][0]){
+            return board[0][0];
+        }
+        if(board[0][1] != SquareState.Empty && board[0][1] == board[1][1] && board[2][1]){
+            return board[0][1];
+        }
+        if(board[0][2] != SquareState.Empty && board[0][2] == board[1][2] && board[2][2]){
+            return board[0][2];
+        }
+        // Diagonals
+        if(board[0][0] != SquareState.Empty && board[0][0] == board[1][1] && board[2][2]){
+            return board[0][0];
+        }
+        if(board[0][2] != SquareState.Empty && board[0][2] == board[1][1] && board[2][0]){
+            return board[0][2];
+        }
+    }
  } 
